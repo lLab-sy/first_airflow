aef280828fa4798d9eb6fa3f3869d8c2dbcfe752
Ball <yongsuwan.sb@gmail.com>
2024-08-25 22:01:19 +0700

    change topic from scraping novel web to match result of valorant

--- dags/valorant_match_result.py
+++ dags/valorant_match_result.py
@@ -5,8 +5,9 @@ using the requests and BeautifulSoup libraries.
 
 source = www.tunwalai.com
 """
-import logging
 from typing import Optional
+import logging
+from bs4 import BeautifulSoup
 import requests
 from requests.exceptions import RequestException, Timeout, HTTPError
 
@@ -28,7 +29,7 @@ def logger_err(msg:str):
     """
     logging.error(msg)
 
-def extract_web_content(url: str) -> Optional[str]:
+def fetch_web_content(url: str) -> Optional[str]:
     """
     Fetches and returns the HTML content of the specified URL.
 
@@ -52,13 +53,48 @@ def extract_web_content(url: str) -> Optional[str]:
         return response.text
 
     except (HTTPError, Timeout) as e:
-        logger_err("Request failed for " + url + ": " + e)
+        logger_err("Request failed for " + url)
         raise
     except RequestException as e:
-        logger_err("An error occurred while fetching data from " + url + ": " + e)
+        logger_err("An error occurred while fetching data from " + url)
         raise
 
-    return None
+
+def extract_web_data(web_data: str) -> Optional[str] :
+    """
+    Extract and returns the HTML content of the web_data.
+
+    Args:
+        web_data (str): The content in web page.
+
+    Returns:
+        Optional[str]: The HTML content of the page as a string, or None if the request fails.
+
+    Raises:
+        ValueError: If the URL is not a valid string.
+        RequestException: For any issues during the HTTP request.
+    """
+    if not isinstance(web_data, str) or not web_data.strip():
+        raise ValueError("The web_data must be a non-empty string.")
+
+    try:
+        web_content = BeautifulSoup(web_data, 'html.parser')
+        # print(web_content.text)
+        pillar_body = web_content.find('div', attrs={'id': 'wrapper'})
+        print(pillar_body.text)
+        # ranking_top3 = pillar_body.find(class_='ranking-top3')
+        # print(ranking_top3)
+        # other_rank = pillar_body.find_all('div', attrs={'class': 'container'})
+        # for novel in top_novel:
+        #     print(novel.text())
+    except:
+        logger_err("someting went wrong")
+        raise
+    return ""
+
+def valorant_match_result():
+    web_data = fetch_web_content('https://www.vlr.gg/matches/results')
+    extract_web_data(web_data)
 
 
-# URL = 'https://www.tunwalai.com/story/type/toplike?period=daily'
+valorant_match_result()
